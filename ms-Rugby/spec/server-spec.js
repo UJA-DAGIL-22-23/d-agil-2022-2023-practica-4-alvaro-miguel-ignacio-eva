a281d6123a8d8c629593ca643ea409a5ec8d5f1e
evaUJA <124682656+evaUJA@users.noreply.github.com>
2023-04-25 13:17:13 +0200

    ms-badminton y ms-plantilla añadido

---
+++ ms-Rugby/spec/server-spec.js
@@ -0,0 +1,90 @@
+/**
+ * @file server-spec.js
+ * @description Fichero con la especificación de las pruebas TDD para server.js del MS MS Plantilla
+ *              Este fichero DEBE llamarse server-spec.js
+ *              Este fichero DEBE ubicarse en el subdirectorio spec/
+ * @author Víctor M. Rivas Santos <vrivas@ujaen.es>
+ * @date 03-Feb-2023
+ */
+
+
+const supertest = require('supertest');
+const assert = require('assert')
+const app = require('../server');
+
+/**
+ * Test para las rutas "estáticas": / y /acerdade
+ */
+describe('Servidor PLANTILLA:', () => {
+  describe('Rutas / y /acercade', () => {
+    it('Devuelve MS Plantilla Home Page', (done) => {
+      supertest(app)
+        .get('/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS Plantilla: home");
+          assert(res.body.autor === "Álvaro Expósito Carrillo");
+          assert(res.body.email === "aec00028@red.ujaen.es");
+          assert(res.body.fecha === "28/03/2023");
+
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+    it('Devuelve MS Plantilla Acerca De', (done) => {
+      supertest(app)
+        .get('/acercade')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS Plantilla: acerca de");
+          assert(res.body.autor === "Álvaro Expósito Carrillo");
+          assert(res.body.email === "aec00028@red.ujaen.es");
+          assert(res.body.fecha === "28/03/2023");
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+  })
+
+  /**
+   * Tests para acceso a la BBDD
+   */
+  describe('Acceso a BBDD:', () => {
+    it('Devuelve Eva al consultar mediante test_db', (done) => {
+      supertest(app)
+        .get('/test_db')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.nombre === "Eva");
+
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+    });
+
+  })
+
+  /**
+     * Tests para ver que todas funciona
+     */
+  it('Devuelve un vector de tamaño 3 al consultar mediante getTodas', (done) => {
+    supertest(app)
+        .get('/getTodas')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+            // console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+            assert(res.body.data.length === 11); //cambio a el numero de jugadores que tiene mi bbdd
+        })
+        .end((error) => { error ? done.fail(error) : done(); }
+        );
+});
+
+});
