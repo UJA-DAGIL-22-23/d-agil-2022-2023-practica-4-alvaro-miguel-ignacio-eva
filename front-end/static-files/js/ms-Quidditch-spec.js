b105f2e97ac5a00998ff246e5ea180318bedbcf4
Álvaro Expósito Carrillo <aec00028@red.ujaen.es>
2023-05-02 13:07:01 +0200

    Corrección Errores Spec Quidditch

--- front-end/static-files/js/ms-Quidditch-spec.js
+++ front-end/static-files/js/ms-Quidditch-spec.js
@@ -8,26 +8,26 @@
 // SPECS para Jasmine
 
 // Constantes para usar en las pruebas
-const elementoTitulo = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_TITULO)
-const elementoContenido = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_CONTENIDO)
-const TITULO_HOME = "Quidditch Home"
-const TITULO_ACERCA_DE = "Quidditch Acerca de"
-const LISTADO_IMPRIMIR_MUCHAS_PERSONAS = "Listado de personas con todos los datos "
-const LISTADO_IMPRIMIR_MUCHAS_PERSONAS_ALFABETICAMENTE="Listado de personas ordenadas alfabeticamente solo con su nombre"
+const elementoTituloQuidditch = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_TITULO)
+const elementoContenidoQuidditch = document.getElementById(Frontend.ID_SECCION_PRINCIPAL_CONTENIDO)
+const TITULO_HOME_Quidditch = "Quidditch Home"
+const TITULO_ACERCA_DE_Quidditch = "Quidditch Acerca de"
+const LISTADO_IMPRIMIR_MUCHAS_PERSONAS_Quidditch = "Listado de personas con todos los datos "
+const LISTADO_IMPRIMIR_MUCHAS_PERSONAS_ALFABETICAMENTE_Quidditch="Listado de personas ordenadas alfabeticamente solo con su nombre"
 const CEREAR = "cerear"
-const LISTADO_NOMBRE_MUCHAS_PERSONAS ="Listado de personas solo con su nombre"
-const LISTADO_UNA_PERSONA="Mostrar una persona"
-const LISTADO_NOMBRE_MUCHAS_PERSONAS_TODOS_DATOS = "Listado de personas ordenadas alfabeticamente con todos sus datos"
+const LISTADO_NOMBRE_MUCHAS_PERSONAS_Quidditch ="Listado de personas solo con su nombre"
+const LISTADO_UNA_PERSONA_Quidditch="Mostrar una persona"
+const LISTADO_NOMBRE_MUCHAS_PERSONAS_TODOS_DATOS_Quidditch = "Listado de personas ordenadas alfabeticamente con todos sus datos"
 
 
-const datosDescargadosPrueba = {
+const datosDescargadosPrueba_Quidditch = {
     mensaje: "Mensaje de prueba descargado",
     autor: "Prueba de autor",
     email: "Prueba de email",
     fecha: "00/00/0000"
 }
 
-const datosDescargadosPruebaPersona = {
+const datosDescargadosPruebaPersona_Quidditch = {
     id: 1,
     nombre: "Harry",
     apellidos: "Potter",
@@ -61,35 +61,40 @@ describe("Quidditch.mostrarHome: ", function () {
     it("muestra datos nulos cuando le pasamos un valor nulo",
         function () {
             Quidditch.mostrarHome()
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
-            expect(elementoContenido.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_HOME_Quidditch
+            )
+            expect(elementoContenidoQuidditch.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
         })
 
     it("muestra datos nulos cuando le pasamos un valor que no es un objeto",
         function () {
             Quidditch.mostrarHome(23)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
-            expect(elementoContenido.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_HOME_Quidditch
+            )
+            expect(elementoContenidoQuidditch.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
         })
 
     it("muestra datos nulos cuando le pasamos un objeto que no tiene campo mensaje",
         function () {
             // Objeto vacío
             Quidditch.mostrarHome({})
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
-            expect(elementoContenido.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_HOME_Quidditch
+            )
+            expect(elementoContenidoQuidditch.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
 
             // Objeto sin campo mensaje
             Quidditch.mostrarHome({ foo: "bar" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
-            expect(elementoContenido.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_HOME_Quidditch
+            )
+            expect(elementoContenidoQuidditch.innerHTML).toBe(Quidditch.datosDescargadosNulos.mensaje)
         })
 
     it("muestra correctamente el título y el mensaje",
         function () {
-            Quidditch.mostrarHome(datosDescargadosPrueba)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_HOME)
-            expect(elementoContenido.innerHTML).toBe(datosDescargadosPrueba.mensaje)
+            Quidditch.mostrarHome(datosDescargadosPrueba_Quidditch)
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_HOME_Quidditch
+            )
+            expect(elementoContenidoQuidditch.innerHTML).toBe(datosDescargadosPrueba_Quidditch.mensaje)
         })
 })
 
@@ -98,50 +103,50 @@ describe("Quidditch.mostrarAcercaDe: ", function () {
     it("muestra datos nulos cuando le pasamos un valor nulo",
         function () {
             Quidditch.mostrarAcercaDe()
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
-            expect(elementoContenido.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
+            expect(elementoContenidoQuidditch.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
         })
 
     it("muestra datos nulos cuando le pasamos un valor que no es un objeto",
         function () {
             Quidditch.mostrarAcercaDe(23)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
-            expect(elementoContenido.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
+            expect(elementoContenidoQuidditch.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
         })
 
     it("muestra datos nulos cuando le pasamos un objeto que no tiene campo mensaje o autor o email o fecha ",
         function () {
             // Objeto vacío
             Quidditch.mostrarAcercaDe({})
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
-            expect(elementoContenido.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
+            expect(elementoContenidoQuidditch.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
 
             // Objeto sin campo mensaje
             Quidditch.mostrarAcercaDe({ autor: "un autor", email: "un email", fecha: "una fecha" })
-           expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
-           expect(elementoContenido.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
+           expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
+           expect(elementoContenidoQuidditch.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
             // Objeto sin campo autor
             Quidditch.mostrarAcercaDe({ mensaje: "un mensaje", email: "un email", fecha: "una fecha" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
-            expect(elementoContenido.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
+            expect(elementoContenidoQuidditch.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeTrue()
             // Objeto sin campo email
             Quidditch.mostrarAcercaDe({ mensaje: "un mensaje", autor: "un autor", fecha: "una fecha" })
-          expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
-           expect(elementoContenido.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeFalse()
+          expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
+           expect(elementoContenidoQuidditch.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeFalse()
             // Objeto sin campo fecha
             Quidditch.mostrarAcercaDe({ mensaje: "un mensaje", autor: "un autor", email: "un email" })
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
-           expect(elementoContenido.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeFalse()
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
+           expect(elementoContenidoQuidditch.innerHTML.search(Quidditch.datosDescargadosNulos.mensaje) >= 0).toBeFalse()
         })
     it("muestra correctamente el título y el mensaje conteniendo el autor, el email y la fecha",
         function () {
-            Quidditch.mostrarAcercaDe(datosDescargadosPrueba)
-            expect(elementoTitulo.innerHTML).toBe(TITULO_ACERCA_DE)
+            Quidditch.mostrarAcercaDe(datosDescargadosPrueba_Quidditch)
+            expect(elementoTituloQuidditch.innerHTML).toBe(TITULO_ACERCA_DE_Quidditch)
 
             // Comprobamos que al buscar el autor, el email y la fecha de prueba los encuentra dentro del contenido del article
-          expect(elementoContenido.innerHTML.search(datosDescargadosPrueba.autor) >= 0).toBeTrue()
-            expect(elementoContenido.innerHTML.search(datosDescargadosPrueba.email) >= 0).toBeTrue()
-            expect(elementoContenido.innerHTML.search(datosDescargadosPrueba.fecha) >= 0).toBeTrue()
+          expect(elementoContenidoQuidditch.innerHTML.search(datosDescargadosPrueba_Quidditch.autor) >= 0).toBeTrue()
+            expect(elementoContenidoQuidditch.innerHTML.search(datosDescargadosPrueba_Quidditch.email) >= 0).toBeTrue()
+            expect(elementoContenidoQuidditch.innerHTML.search(datosDescargadosPrueba_Quidditch.fecha) >= 0).toBeTrue()
         })
 
 })
@@ -150,13 +155,13 @@ describe("Quidditch.imprimeMuchasPersonas: ", function () {
     it("muestra datos nulos cuando le pasamos un valor nulo",
         function () {
             Quidditch.imprimeMuchasPersonas([])
-            expect(elementoTitulo.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS)
+            expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS_Quidditch)
 
         })
     it("muestra datos nulos cuando le pasamos un valor no nulo ",
         function () {
             Quidditch.imprimeMuchasPersonas(15)
-            expect(elementoTitulo.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS)
+            expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS_Quidditch)
 
         })
 })
@@ -165,13 +170,13 @@ describe("Quidditch.imprimeOrdenadorAlfabeticamente: ", function (){
     it("muestra datos nulos cuando le pasamos un valor nulo",
         function () {
             Quidditch.imprimeOrdenadorAlfabeticamente([])
-            expect(elementoTitulo.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS_ALFABETICAMENTE)
+            expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS_ALFABETICAMENTE_Quidditch)
 
         })
     it("muestra datos nulos cuando le pasamos un valor no nulo ",
         function () {
             Quidditch.imprimeOrdenadorAlfabeticamente(15)
-            expect(elementoTitulo.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS_ALFABETICAMENTE)
+            expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_IMPRIMIR_MUCHAS_PERSONAS_ALFABETICAMENTE_Quidditch)
 
         })
 })
@@ -190,13 +195,13 @@ describe("Quidditch.imprimeNombreMuchasPersonas: ", function (){
     it("muestra datos nulos cuando le pasamos un valor nulo",
         function () {
             Quidditch.imprimeNombreMuchasPersonas([])
-            expect(elementoTitulo.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS)
+            expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS_Quidditch)
 
         })
     it("muestra datos nulos cuando le pasamos un valor no nulo ",
         function () {
             Quidditch.imprimeNombreMuchasPersonas(15)
-            expect(elementoTitulo.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS)
+            expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS_Quidditch)
 
         })
 })
@@ -205,13 +210,13 @@ describe("Quidditch.imprimeOrdenadorAlfabeticamenteTodosCampos", function() {
         it("muestra datos nulos cuando le pasamos un valor nulo",
             function () {
                 Quidditch.imprimeOrdenadorAlfabeticamenteTodosCampos([])
-                expect(elementoTitulo.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS_TODOS_DATOS)
+                expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS_TODOS_DATOS_Quidditch)
 
             })
         it("muestra datos nulos cuando le pasamos un valor no nulo ",
             function () {
                 Quidditch.imprimeOrdenadorAlfabeticamenteTodosCampos(15)
-                expect(elementoTitulo.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS_TODOS_DATOS)
+                expect(elementoTituloQuidditch.innerHTML).toBe(LISTADO_NOMBRE_MUCHAS_PERSONAS_TODOS_DATOS_Quidditch)
 
             })
     })
